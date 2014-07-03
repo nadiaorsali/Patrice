24b869d0787d3a5a7178cd2b7a8a2fc72854ac5c
nicolas <machut.nicolas@gmail.com>
2014-07-03 14:56:23 +0200

    correction (je pense) du problème de mémoire avec hibernate

--- src/main/java/common/SessionHibernate.java
+++ src/main/java/common/SessionHibernate.java
@@ -5,9 +5,12 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.classic.Session;
 
+import javax.ejb.Singleton;
+
 /**
  * Created by nicolas on 21/05/14.
  */
+@Singleton
 public class SessionHibernate {
     private SessionFactory factory;
     private Session session;
--- src/main/java/dao/AbstractDao.java
+++ src/main/java/dao/AbstractDao.java
@@ -4,6 +4,7 @@ import common.SessionHibernate;
 import org.hibernate.classic.Session;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -11,37 +12,48 @@ import java.util.List;
  */
 abstract class AbstractDao<T, PK extends Serializable> implements DAO<T, PK> {
 
-    protected Session session = new SessionHibernate().getInstance();
-
 
     public T get(final Class<T> type, PK id) {
+        Session session = new SessionHibernate().getInstance();
         org.hibernate.Transaction transaction = session.beginTransaction();
-        return (T) session.get(type, id);
+        T obj = (T) session.get(type, id);
+        session.close();
+        return obj;
     }
 
     public T save(T obj) {
+        Session session = new SessionHibernate().getInstance();
         org.hibernate.Transaction transaction = session.beginTransaction();
         session.save(obj);
         transaction.commit();
+        session.close();
         return obj;
     }
 
     public T update(T obj) {
+        Session session = new SessionHibernate().getInstance();
         org.hibernate.Transaction transaction = session.beginTransaction();
         session.saveOrUpdate(obj);
         transaction.commit();
+        session.close();
         return obj;
     }
 
     public List<T> list(final Class<T> type) {
+        List<T> list = new ArrayList<>();
+        Session session = new SessionHibernate().getInstance();
         org.hibernate.Transaction transaction = session.beginTransaction();
-        return session.createCriteria(type).list();
+        list = session.createCriteria(type).list();
+        session.close();
+        return list;
     }
 
     public void delete(T obj) {
 
+        Session session = new SessionHibernate().getInstance();
         org.hibernate.Transaction transaction = session.beginTransaction();
         session.delete(obj);
         transaction.commit();
+        session.close();
     }
 }
